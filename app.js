341a45f6b98d6181ad3af004ae0d1ce9f0c2395e
gwuiinkim <gwuiinkim@gmail.com>
2019-05-08 00:09:38 +0900

    saving the image

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "#2c2c2c";
 const CANVAS_SIZE = 700;
@@ -11,6 +12,10 @@ canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 //  canvas에 css로 사이즈를 정해줄 뿐만 아니라,
 //  element 자체에 사이즈를 정해줘야 잘 작동함.
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+//  initialize canvas background color
 
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
@@ -74,12 +79,25 @@ function handleCanvasClick() {
   }
 }
 
+function handleSaveClick() {
+  const image = canvas.toDataURL();
+  const link = document.createElement("a");
+  link.href = image;
+  link.download = "Paint by JS";
+  link.click();
+}
+
+function handleCM() {
+  event.preventDefault();
+}
+
 function init() {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", onMouseUp);
   canvas.addEventListener("mouseleave", stopPainting);
   canvas.addEventListener("click", handleCanvasClick);
+  canvas.addEventListener("contextmenu", handleCM);
 }
 
 Array.from(colors).forEach(color =>
@@ -96,4 +114,8 @@ if (range) {
 
 if (mode) {
   mode.addEventListener("click", handleModeClick);
+}
+
+if (saveBtn) {
+  saveBtn.addEventListener("click", handleSaveClick);
 }
