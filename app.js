9b6ffdee29b6718801da6ceba1f2c8f24dd5d85a
gwuiinkim <gwuiinkim@gmail.com>
2019-05-07 22:28:38 +0900

    2D context canvas settings

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext('2d');
+
+canvas.width = 700;
+canvas.height = 700;
+//  canvas에 css로 사이즈를 정해줄 뿐만 아니라, 
+//  element 자체에 사이즈를 정해줘야 잘 작동함.
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.linewidth = 2.5;
 
 let painting = false;
 
@@ -8,10 +17,23 @@ function stopPainting() {
 
 function onMouseMove(event) {
     const x = event.offsetX;
-    const y = event.offsetY
+    const y = event.offsetY;
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+    } else {
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    }
+    // Canvas를 어느 정도 이해를 해야하는 부분
+    //  마우스가 눌리지 않을 때에는 path를 계속 만들다가도,
+    // 마우스가 눌릴 때에 그 path만을 이용하여
+    // 그리는 개념.
 }
 
-function onMouseDown(event){
+
+
+function startPainting(event){
     painting=true;
 }
 
@@ -21,7 +43,7 @@ function onMouseUp(event){
 
 function init {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", onMouseUp);
     canvas.addEventListener("mouseleave", stopPainting);
 }
